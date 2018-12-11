6104af963d0a8a820324fc3473073bfb29f4981b
djerman <djerman@pobox.com>
2018-12-11 09:36:38 -0500

    switched to jwx for tokens

--- gdo-api/token-authorizer/go.mod
+++ gdo-api/token-authorizer/go.mod
@@ -2,6 +2,10 @@ module github.com/djerman3/gdo/gdo-api/token-authorizer
 
 require (
 	github.com/aws/aws-lambda-go v1.8.0
-	golang.org/x/crypto v0.0.0-20181203042331-505ab145d0a9 // indirect
-	gopkg.in/square/go-jose.v2 v2.2.1
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/lestrrat-go/jwx v0.0.0-20180928232350-0d477e6a1f0e
+	github.com/lestrrat-go/pdebug v0.0.0-20180220043849-39f9a71bcabe // indirect
+	github.com/pkg/errors v0.8.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2 // indirect
 )
--- gdo-api/token-authorizer/main.go
+++ gdo-api/token-authorizer/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"gopkg.in/square/go-jose.v2/jwt"
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
 )
 
+// global token decrypt key
+var pubKey *jwk.Key
+
 // Help function to generate an IAM policy
-func generatePolicy(principalId, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
-	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalId}
+func generatePolicy(principalID, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
+	authResponse := events.APIGatewayCustomAuthorizerResponse{PrincipalID: principalID}
 
 	if effect != "" && resource != "" {
 		authResponse.PolicyDocument = events.APIGatewayCustomAuthorizerPolicy{
@@ -38,23 +45,22 @@ func generatePolicy(principalId, effect, resource string) events.APIGatewayCusto
 }
 
 func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
-	token := event.AuthorizationToken
-	parsed, err := jwt.ParseSigned(token) // decode token
+	payload := event.AuthorizationToken
+	token, err := jwt.ParseVerify(bytes.NewReader([]byte(payload)), jwa.RS256, pubKey)
 	if err != nil {
+		// invalid parseverify
 		// for now authorize
 		fmt.Println(err)
 		return generatePolicy("user", "Allow", event.MethodArn), nil
 		//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 	}
-	c := make(map[string]interface{})
-	pubKey := os.Getenv("PUBKEY")
-	err = parsed.Claims(&pubKey, &c)
-	if err != nil {
-		// for now authorize
-		fmt.Println(err)
-		return generatePolicy("user", "Allow", event.MethodArn), nil
-		//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
-	}
+	user := token.Subject()
+	audience := token.Audience()
+	expire := token.Expiration()
+	// for now authorize
+	fmt.Printf("Accepted token i:%s a:%s u:%s e:%s\n", token.Issuer(), audience, user, expire.Format(time.RFC3339))
+	return generatePolicy("user", "Allow", event.MethodArn), nil
+	//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 	//test claims
 	if false {
 		return generatePolicy("user", "Deny", event.MethodArn), nil
@@ -67,11 +73,25 @@ func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerR
 		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
 	}
 	// for now authorize
-	fmt.Printf("auth - no errors iss: %s, sub: %s\n", c["iss"], c["sub"])
+	fmt.Printf("auth - no errors iss: %s, sub: %s\n", token.Issuer(), token.Subject())
 	return generatePolicy("user", "Allow", event.MethodArn), nil
 
 }
 
 func main() {
+	keyserver := os.Getenv("JWKS_URL")
+	set, err := jwk.Fetch(keyserver)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	keys := set.LookupKeyID(os.Getenv("KEY_ID"))
+
+	if len(keys) == 0 {
+		fmt.Println("no key by that id")
+		keys = set.Keys
+	}
+	pubKey = &keys[0]
 	lambda.Start(handleRequest)
 }
