4f2b9cf1cb234f687228ddfb1765474ec72bac08
djerman <djerman@pobox.com>
2018-12-17 23:02:58 -0500

    taking in lessons from another demo

--- gdo-api/token-authorizer/main.go
+++ gdo-api/token-authorizer/main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,8 +13,8 @@ import (
 	"github.com/lestrrat-go/jwx/jwt"
 )
 
-// global token decrypt key
-var pubKey *jwk.Key
+const tokISSUER = "https://jerman3.auth0.com/"
+const tokAUDIENCE = "https://gdo.heather.com"
 
 // Help function to generate an IAM policy
 func generatePolicy(principalID, effect, resource string) events.APIGatewayCustomAuthorizerResponse {
@@ -35,63 +33,73 @@ func generatePolicy(principalID, effect, resource string) events.APIGatewayCusto
 		}
 	}
 
-	// Optional output with custom properties of the String, Number or Boolean type.
-	authResponse.Context = map[string]interface{}{
-		"stringKey":  "stringval",
-		"numberKey":  123,
-		"booleanKey": true,
-	}
 	return authResponse
 }
 
 func handleRequest(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	// Find a key
+	keyserver := os.Getenv("JWKS_URL")
+	if len(keyserver) == 0 {
+		// invalid url
+		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Error: Invalid keyserver var")
+	}
+	set, err := jwk.Fetch(keyserver)
+	if err != nil {
+		// invalid url
+		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Error: Invalid keyserver:%s", err)
+	}
+	// decode key
+	keyId := os.Getenv("JWK_ID")
+	keys := set.Keys
+	if len(keyId) > 0 {
+		keys = set.LookupKeyID(keyId)
+		if len(keys) == 0 {
+			return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Error: Invalid keyId:%s", keyId)
+		}
+	}
+	key, err := set.Keys[0].Materialize()
+	if err != nil {
+		// invalid key
+		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Error: Invalid key:%s", err)
+	}
+
+	//parse
 	payload := event.AuthorizationToken
-	token, err := jwt.ParseVerify(bytes.NewReader([]byte(payload)), jwa.RS256, pubKey)
+	token, err := jwt.ParseVerify(bytes.NewReader([]byte(payload)), jwa.RS256, key)
 	if err != nil {
 		// invalid parseverify
-		// for now authorize
-		fmt.Println(err)
-		return generatePolicy("user", "Allow", event.MethodArn), nil
-		//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+		return events.APIGatewayCustomAuthorizerResponse{}, fmt.Errorf("Error: Invalid token:%s", err)
 	}
-	user := token.Subject()
-	audience := token.Audience()
-	expire := token.Expiration()
-	// for now authorize
-	fmt.Printf("Accepted token i:%s a:%s u:%s e:%s\n", token.Issuer(), audience, user, expire.Format(time.RFC3339))
-	return generatePolicy("user", "Allow", event.MethodArn), nil
-	//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
-	//test claims
-	if false {
+	if token.Issuer() != tokISSUER {
+		// wrong claims
 		return generatePolicy("user", "Deny", event.MethodArn), nil
 	}
+	authResponse := generatePolicy("user", "Allow", event.MethodArn)
+	// Optional output with custom properties of the String, Number or Boolean type.
+	authResponse.Context = map[string]interface{}{
+		"subject":  token.Subject(),
+		"issuer":   token.Issuer(),
+		"audience": token.Audience(),
+		"issueAt":  token.IssuedAt(),
+		"expires":  token.Expiration(),
+	}
+	return authResponse, nil
+	//return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+	//test claims to return deny,err
+	//if false {
+	//	return generatePolicy("user", "Deny", event.MethodArn), nil
+	//}
 	// check authority
-	if false {
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
-	}
-	if err != nil {
-		return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
-	}
-	// for now authorize
-	fmt.Printf("auth - no errors iss: %s, sub: %s\n", token.Issuer(), token.Subject())
-	return generatePolicy("user", "Allow", event.MethodArn), nil
+	//if false {
+	//	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Unauthorized") // Return a 401 Unauthorized response
+	//	}
+	//if err != nil {
+	//	return events.APIGatewayCustomAuthorizerResponse{}, errors.New("Error: Invalid token")
+	//}
 
 }
 
 func main() {
-	keyserver := os.Getenv("JWKS_URL")
-	set, err := jwk.Fetch(keyserver)
 
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	keys := set.LookupKeyID(os.Getenv("KEY_ID"))
-
-	if len(keys) == 0 {
-		fmt.Println("no key by that id")
-		keys = set.Keys
-	}
-	pubKey = &keys[0]
 	lambda.Start(handleRequest)
 }
