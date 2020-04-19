19a12e32f35728152461a3fdd67f2e4a0dff5f21
djerman <djerman@pobox.com>
2020-04-19 03:45:36 -0400

    fix the bad btn

--- gdapi/gdApi.go
+++ gdapi/gdApi.go
@@ -113,10 +113,10 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 					"		<div class=\"jumbotron text-center\"><h1>Garage Door</h1>" +
 					"		<p>The door state is " + state + "</p></div>" +
 					"       <form action=\"/\" method=\"POST\">" +
-					"        <div class=\"row\"><div class=\"col-sm-4\">" +
+					"        <div class=\"row\"><div class=\"col-sm-2\">" +
 					"			<button class=\"btn btn-primary btn-lg btn-block\" type=\"submit\" formaction=\"/\" autofocus=\"autofocus\">CLICK</button>" +
-					"        </div><div class=\"col-sm-4\">" +
-					"			<button class=\"btn btn-secondary btn-lg.btn-block\" type=\"reset\" formaction=\"/\" >RELOAD</button>" +
+					"        </div><div class=\"col-sm-2\">" +
+					"			<button class=\"btn btn-secondary btn-lg btn-block\" type=\"reset\" formaction=\"/\" >RELOAD</button>" +
 					"		</div></div>" +
 					"		</form>" +
 					"	</body>"))
